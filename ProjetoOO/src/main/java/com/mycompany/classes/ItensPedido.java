df554c1ebca0340120a646fb93b2d8e576c6e833
Laura Fernandes <scaecMRE@gmail.com>
2022-06-27 16:52:34 -0300

    Create ItensPedido.java

---
+++ ProjetoOO/src/main/java/com/mycompany/classes/ItensPedido.java
@@ -0,0 +1,163 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classes;
+
+import com.mycompany.conexao.EntidadeBase;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Usuario
+ */
+@Entity
+@Table(name = "itens_pedido")
+@NamedQueries({
+    @NamedQuery(name = "ItensPedido.findAll", query = "SELECT i FROM ItensPedido i")})
+public class ItensPedido implements Serializable, EntidadeBase {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "id_produto")
+    private Produto idProduto;
+    @Column(name = "quantidade")
+    private Integer quantidade;
+    @Column(name = "massa")
+    private String massa;
+    @Column(name = "recheio")
+    private String recheio;
+    @Column(name = "cobertura")
+    private String cobertura;
+    @Column(name = "tamanho")
+    private Character tamanho;
+    @Basic(optional = false)
+    @Column(name = "descricao")
+    private String descricao;
+    @JoinColumn(name = "id_pedido", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Pedido idPedido;
+
+    public ItensPedido() {
+    }
+
+    public ItensPedido(Integer id) {
+        this.id = id;
+    }
+
+    public ItensPedido(Integer id, int idProduto, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Produto getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Produto idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getMassa() {
+        return massa;
+    }
+
+    public void setMassa(String massa) {
+        this.massa = massa;
+    }
+
+    public String getRecheio() {
+        return recheio;
+    }
+
+    public void setRecheio(String recheio) {
+        this.recheio = recheio;
+    }
+
+    public String getCobertura() {
+        return cobertura;
+    }
+
+    public void setCobertura(String cobertura) {
+        this.cobertura = cobertura;
+    }
+
+    public Character getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(Character tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Pedido getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(Pedido idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ItensPedido)) {
+            return false;
+        }
+        ItensPedido other = (ItensPedido) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.classes.ItensPedido[ id=" + id + " ]";
+    }
+    
+}
