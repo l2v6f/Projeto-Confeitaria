3aee5f8aaab4a31932b233ce8935f32609ca5140
Laura Fernandes <scaecMRE@gmail.com>
2022-06-27 16:53:04 -0300

    Create Telefone.java

---
+++ ProjetoOO/src/main/java/com/mycompany/classes/Telefone.java
@@ -0,0 +1,137 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classes;
+
+import com.mycompany.conexao.EntidadeBase;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Usuario
+ */
+@Entity
+@Table(name = "telefone")
+@NamedQueries({
+    @NamedQuery(name = "Telefone.findAll", query = "SELECT t FROM Telefone t")})
+public class Telefone implements Serializable,EntidadeBase{
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "numero")
+    private String numero;
+    @Basic(optional = false)
+    @Column(name = "ddd")
+    private String ddd;
+    @Column(name = "observacao")
+    private String observacao;
+    @Basic(optional = false)
+    @Column(name = "tipo")
+    private String tipo;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idTelefone")
+    private List<Usuario> usuarioList;
+
+    public Telefone() {
+    }
+
+    public Telefone(Integer id) {
+        this.id = id;
+    }
+
+    public Telefone(Integer id, String numero, String ddd, String tipo) {
+        this.id = id;
+        this.numero = numero;
+        this.ddd = ddd;
+        this.tipo = tipo;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public List<Usuario> getUsuarioList() {
+        return usuarioList;
+    }
+
+    public void setUsuarioList(List<Usuario> usuarioList) {
+        this.usuarioList = usuarioList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Telefone)) {
+            return false;
+        }
+        Telefone other = (Telefone) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.classes.Telefone[ id=" + id + " ]";
+    }
+    
+}
