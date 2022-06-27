dc8764866279a4fdb2df5fa1d68004e3ba332efb
Laura Fernandes <scaecMRE@gmail.com>
2022-06-27 16:52:22 -0300

    Create Endereco.java

---
+++ ProjetoOO/src/main/java/com/mycompany/classes/Endereco.java
@@ -0,0 +1,193 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classes;
+
+import com.mycompany.conexao.EntidadeBase;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Usuario
+ */
+@Entity
+@Table(name = "endereco")
+@NamedQueries({
+    @NamedQuery(name = "Endereco.findAll", query = "SELECT e FROM Endereco e")})
+public class Endereco implements Serializable, EntidadeBase{
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "logradouro")
+    private String logradouro;
+    @Basic(optional = false)
+    @Column(name = "quadra")
+    private String quadra;
+    @Column(name = "lote")
+    private String lote;
+    @Basic(optional = false)
+    @Column(name = "cidade")
+    private String cidade;
+    @Basic(optional = false)
+    @Column(name = "estado")
+    private String estado;
+    @Basic(optional = false)
+    @Column(name = "cep")
+    private String cep;
+    @Column(name = "observacao")
+    private String observacao;
+    @Basic(optional = false)
+    @Column(name = "bairro")
+    private String bairro;
+    @Column(name = "complemento")
+    private String complemento;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idEndereco")
+    private List<Usuario> usuarioList;
+
+    public Endereco() {
+    }
+
+    public Endereco(Integer id) {
+        this.id = id;
+    }
+
+    public Endereco(Integer id, String logradouro, String quadra, String cidade, String estado, String cep, String bairro) {
+        this.id = id;
+        this.logradouro = logradouro;
+        this.quadra = quadra;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.cep = cep;
+        this.bairro = bairro;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getQuadra() {
+        return quadra;
+    }
+
+    public void setQuadra(String quadra) {
+        this.quadra = quadra;
+    }
+
+    public String getLote() {
+        return lote;
+    }
+
+    public void setLote(String lote) {
+        this.lote = lote;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public List<Usuario> getUsuarioList() {
+        return usuarioList;
+    }
+
+    public void setUsuarioList(List<Usuario> usuarioList) {
+        this.usuarioList = usuarioList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Endereco)) {
+            return false;
+        }
+        Endereco other = (Endereco) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.classes.Endereco[ id=" + id + " ]";
+    }
+    
+}
