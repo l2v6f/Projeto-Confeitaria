ee67c9d92d9f725e995dcef7640eab604cda581a
Laura Fernandes <scaecMRE@gmail.com>
2022-06-27 16:52:59 -0300

    Create Status.java

---
+++ ProjetoOO/src/main/java/com/mycompany/classes/Status.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.classes;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Usuario
+ */
+@Entity
+@Table(name = "status")
+@NamedQueries({
+    @NamedQuery(name = "Status.findAll", query = "SELECT s FROM Status s")})
+public class Status implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "descricao")
+    private String descricao;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idStatus")
+    private List<HistoricoPedido> historicoPedidoList;
+
+    public Status() {
+    }
+
+    public Status(Integer id) {
+        this.id = id;
+    }
+
+    public Status(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<HistoricoPedido> getHistoricoPedidoList() {
+        return historicoPedidoList;
+    }
+
+    public void setHistoricoPedidoList(List<HistoricoPedido> historicoPedidoList) {
+        this.historicoPedidoList = historicoPedidoList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Status)) {
+            return false;
+        }
+        Status other = (Status) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.classes.Status[ id=" + id + " ]";
+    }
+    
+}
